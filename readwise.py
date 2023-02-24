605df0ba699c4ad4dd7c4c58e677761e70df21b0
Alex Bowe <alex@alexbowe.com>
2023-02-23 22:47:43 -0800

    add logging

--- readwise.py
+++ readwise.py
@@ -1,6 +1,9 @@
 import requests
+import datetime
+import logging
 from dataclasses import dataclass
 
+MODULE_NAME = __name__.split('.')[-1]
 
 @dataclass
 class ReadwiseHighlight:
@@ -50,32 +53,60 @@ class ReadwiseDocument:
 class ReadwiseClient:
     def __init__(self, api_key: str=None):
         self._base_url = f"https://readwise.io/api/v2"
+        self._parent_logger = None
+        self._logger = logging.getLogger(MODULE_NAME)
+        self.latest_fetch_time = None
         self.set_api_key(api_key)
+
+    def set_parent_logger(self, parent_logger):
+        self._parent_logger = parent_logger
+        self._logger = self._parent_logger.getChild(MODULE_NAME)
+        return self
 
     def set_api_key(self, api_key):
         self._api_key = api_key
         return self
     
+    def _update_time(self):
+        self.latest_fetch_time = datetime.datetime.now()
+    
     # Taken from https://readwise.io/api_deets
     def export(self, updated_after=None):
-        # If you want to get new highlights updated since your last fetch of allData, do this.
-        # #last_fetch_was_at = datetime.datetime.now() - datetime.timedelta(days=1)  # use your own stored date
-        # new_data = fetch_from_export_api(last_fetch_was_at.isoformat())
+        self._logger.info("Exporting Readwise data")
+        self._update_time()
         full_data = []
         next_page_cursor = None
         while True:
             params = {}
             if next_page_cursor: params["pageCursor"] = next_page_cursor
             if updated_after: params["updatedAfter"] = updated_after
+            self._logger.debug(f"Making Readwise export API request with params={params}")
             response = requests.get(
                 url=f"{self._base_url}/export/",
                 params=params,
                 headers={"Authorization": f"Token {self._api_key}"},
                 verify=True)
-            json_data = response.json()
-            collection = json_data["results"]
-            full_data.extend(ReadwiseDocument(**d) for d in collection)
+            try:
+                response.raise_for_status()
+                json_data = response.json()
+                results = json_data["results"]
+            except Exception as e:
+                self._logger.debug({"response": response.text}, exc_info=e)
+                self._logger.exception(e, exc_info=e)
+                raise e
+            full_data.extend(ReadwiseDocument(**d) for d in results)
+            self._logger.debug(f"Fetched {len(results)} documents in this page")
             next_page_cursor = json_data.get("nextPageCursor")
-            if not next_page_cursor:
-                break
+            if not next_page_cursor: break
+        self._logger.info("Finished exporting Readwise data")
+        self._logger.debug(f"Fetched {len(full_data)} documents in total")
+        num_doc_notes = sum(1 for d in full_data if d.document_note)
+        self._logger.debug(f"Fetched {num_doc_notes} document notes in total")
+        num_highlights = sum(len(d.highlights) for d in full_data if d.highlights)
+        self._logger.debug(f"Fetched {num_highlights} highlights in total")
         return full_data
+    
+    def updates(self):
+        if not self.latest_fetch_time: return self.export()
+        self._update_time()
+        return self.export(updated_after=self.latest_fetch_time.isoformat())
